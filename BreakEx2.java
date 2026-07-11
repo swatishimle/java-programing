e8518dfa4145688ba3b2d09fb590ca945212d7bf
swati shimle <swatishimle@gmail.com>
2026-07-11 20:44:06 +0530

    Added BreakEx2.java

---
+++ BreakEx2.java
@@ -0,0 +1,12 @@
+public class BreakEx2 {
+    public static void main(String[] args) {
+		for(int a=1;a<=5;a++) {
+			System.out.println("Swati");
+			if(a==3){
+				break;
+			}
+			System.out.println("Shimle");
+		}
+		System.out.println("Bye");
+	}
+}
